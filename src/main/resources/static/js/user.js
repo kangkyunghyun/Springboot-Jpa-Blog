9ec18e646248f64d83acd0a56ca9a0f00288308c
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-06-16 01:34:54 +0900

    feat: 요청 주소 변경

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -23,14 +23,14 @@ let index = {
         // ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환
         $.ajax({
             type: "POST",
-            url: "/blog/api/user",
+            url: "/api/user",
             data: JSON.stringify(data), // http body 데이터
             contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
             dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript 오브젝트로 변경
         }).done(function (res) {
             alert("회원가입이 완료되었습니다.");
             // console.log(res);
-            location.href = "/blog";
+            location.href = "/";
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
@@ -45,14 +45,14 @@ let index = {
 
         $.ajax({
             type: "POST",
-            url: "/blog/api/user/login",
+            url: "/api/user/login",
             data: JSON.stringify(data), // http body 데이터
             contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
             dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript 오브젝트로 변경
         }).done(function (res) {
             alert("로그인이 완료되었습니다.");
             // console.log(res);
-            location.href = "/blog";
+            location.href = "/";
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
