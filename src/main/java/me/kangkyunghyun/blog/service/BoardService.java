c64d9021afae16cee2d9cfe601efb7789d63b3ef
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-06-21 01:42:52 +0900

    feat: 글 목록 보기

--- src/main/java/me/kangkyunghyun/blog/service/BoardService.java
+++ src/main/java/me/kangkyunghyun/blog/service/BoardService.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -22,5 +24,9 @@ public class BoardService {
         board.setCount(0);
         board.setUser(user);
         boardRepository.save(board);
+    }
+
+    public List<Board> 글목록() {
+        return boardRepository.findAll();
     }
 }
