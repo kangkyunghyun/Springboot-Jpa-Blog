5ee0c321cc6eda71e012d465fb8150ba55ffdd40
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-06-21 21:44:51 +0900

    feat: 글 상세보기, 삭제, 수정

--- src/main/java/me/kangkyunghyun/blog/controller/BoardController.java
+++ src/main/java/me/kangkyunghyun/blog/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -21,6 +22,18 @@ public class BoardController {
     public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         model.addAttribute("boards", boardService.글목록(pageable));
         return "index";
+    }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/detail";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/updateForm";
     }
 
     // USER 권한이 필요
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,12 @@ let index = {
         $("#btn-save").on("click", ()=>{
             this.save();
         });
+        $("#btn-delete").on("click", ()=>{
+            this.deleteById();
+        });
+        $("#btn-update").on("click", ()=>{
+            this.update();
+        });
     },
 
     save: function () {
@@ -19,12 +25,48 @@ let index = {
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         }).done(function (res) {
-            alert("글쓰기가 완료되었습니다.");
+            alert("글 작성이 완료되었습니다.");
             location.href = "/";
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
-    }
+    },
+
+    deleteById: function () {
+        let id = $("#id").text();
+        $.ajax({
+            type: "DELETE",
+            url: "/api/board/"+id,
+            dataType: "json"
+        }).done(function (res) {
+            alert("글 삭제가 완료되었습니다.");
+            location.href = "/";
+        }).fail(function (err) {
+            alert(JSON.stringify(err));
+        });
+    },
+
+    update: function () {
+        let id = $("#id").val();
+
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        $.ajax({
+            type: "PUT",
+            url: "/api/board/"+id,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (res) {
+            alert("글 수정이 완료되었습니다.");
+            location.href = "/";
+        }).fail(function (err) {
+            alert(JSON.stringify(err));
+        });
+    },
 }
 
 index.init();
