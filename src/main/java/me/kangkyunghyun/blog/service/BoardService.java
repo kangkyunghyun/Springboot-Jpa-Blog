7429fea980d8ba0d34e06b219dff5688757dbd80
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-11-21 11:59:03 +0900

    fix: AI 요약 개행 변경

--- src/main/java/me/kangkyunghyun/blog/service/BoardService.java
+++ src/main/java/me/kangkyunghyun/blog/service/BoardService.java
@@ -36,6 +36,10 @@ public class BoardService {
     @Transactional
     public void 글쓰기(Board board, User user) {
         String summary = geminiService.getSummary(board.getContent());
+        String newContent = "[AI 3줄 요약]\n" + summary + "\n\n\n" + board.getContent();
+        if (newContent != null) {
+            newContent = newContent.replaceAll("(\r\n | \r | \n | \n\r)", "<br/>");
+        }
         board.setCount(0);
         board.setUser(user);
         board.setContent("[AI 3줄 요약]\n" + summary + "\n\n\n" + board.getContent());
